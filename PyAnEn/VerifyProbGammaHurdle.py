eb429af5d90c16b456ff301c6955a02deb001e9a
Weiming Hu <huweiming950714@gmail.com>
2022-03-29 09:13:13 -0700

    fix integration range for gamma hurdle

--- PyAnEn/VerifyProbGammaHurdle.py
+++ PyAnEn/VerifyProbGammaHurdle.py
@@ -67,14 +67,14 @@ class VerifyProbGammaHurdle(VerifyProb):
         assert not self.truncated, 'Truncation is currently not implemented.'
         
     def _prob_to_variance(self):
-        return integrate(verifier=self, type='variance', nbins=self.n_approx_bins, integration_range=None)
+        return integrate(verifier=self, type='variance', nbins=self.n_approx_bins, integration_range=self.integration_range)
     
     def _cdf(self, over=None, below=None):
         return cdf_gamma_hurdle(self.f['pop'], self.f['mu'], self.f['sigma'], over=over, below=below)
     
     def _prob_to_determ(self):
-        return integrate(verifier=self, type='mean', nbins=self.n_approx_bins, integration_range=None)
+        return integrate(verifier=self, type='mean', nbins=self.n_approx_bins, integration_range=self.integration_range)
     
     def _crps(self):
-        return integrate(verifier=self, type='brier', nbins=self.n_approx_bins, integration_range=None)
+        return integrate(verifier=self, type='brier', nbins=self.n_approx_bins, integration_range=self.integration_range)
     
